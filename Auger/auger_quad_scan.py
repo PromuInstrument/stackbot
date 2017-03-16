4b4e7e1891adbd73bf8096e0e1fa3bf9dd9b128c
Edward Barnard <esbarnard@lbl.gov>
2017-03-15 20:38:55 -0700

    modes to scan x1/y1, x2/y2, x1/x1 or y1/y2, also smoothing of data before finding local max, also bug fixes

--- Auger/auger_quad_scan.py
+++ Auger/auger_quad_scan.py
@@ -3,6 +3,7 @@
 from __future__ import division
 import pyqtgraph as pg
 import numpy as np
+from scipy import ndimage
 from qtpy import QtWidgets
 import time
 
@@ -17,8 +18,18 @@ class AugerQuadSlowScan(BaseRaster2DSlowScan):
     
     name = "auger_quad_slowscan"
     
+    q1 = {'x_slow':'quad_X1', 'x_fast':'X1','y_slow':'quad_Y1', 'y_fast':'Y1'}
+    q2 = {'x_slow':'quad_X2', 'x_fast':'X2','y_slow':'quad_Y2', 'y_fast':'Y2'}
+    xx = {'x_slow':'quad_X1', 'x_fast':'X1','y_slow':'quad_X2', 'y_fast':'X2'}
+    yy = {'x_slow':'quad_Y1', 'x_fast':'Y1','y_slow':'quad_Y2', 'y_fast':'Y2'}
+    scan_mode = [q1,q2,xx,yy]
+
+    
     def scan_specific_setup(self):
         
+        self.settings.New('quad_scan_mode', dtype=int, initial = 0, vmin = 0, vmax = 3, 
+                          choices=(('quad 1',0),('quad 2',1),('x shift/angle',2),('y shift/angle',3)))
+
         #auger analyzer
         self.settings.New('ke_start', dtype=float, initial=30,unit = 'V',vmin=0,vmax = 2200)
         self.settings.New('ke_end',   dtype=float, initial=600,unit = 'V',vmin=1,vmax = 2200)
@@ -42,12 +53,9 @@ class AugerQuadSlowScan(BaseRaster2DSlowScan):
         self.set_v_limits(-50,50, set_scan_to_max=True)
         
         
-        
-    
-        
     # begining of run
     def pre_scan_setup(self):
-        print("pre_scan_setup")
+        print("pre_scan_setup auger_quad")
 
         #### Reset FPGA
         self.auger_fpga_hw.settings['int_trig_sample_count'] = 0 
@@ -65,25 +73,33 @@ class AugerQuadSlowScan(BaseRaster2DSlowScan):
         
         self.sample_block = 21
         self.timeout = 2.0 * self.settings['dwell']
-        print("pre_scan_setup2")
+        print("pre_scan_setup2 auger_quad")
 
         self.auger_fpga_hw.settings['period'] =self.settings['dwell']/(self.sample_block - 1)
         self.auger_fpga_hw.settings['trigger_mode'] = 'int'
-        print("pre_scan_setup")
+        print("pre_scan_setup done auger_quad")
 
 
-    def move_position_start(self, x,y):        
-        self.move_position_slow(x, y, 0,0)
-        self.analyzer_hw.settings['quad_X1'] = x
-        self.analyzer_hw.settings['quad_Y1'] = y
+    def move_position_start(self, x,y):       
+        qmode = self.settings['quad_scan_mode']
+        XX = self.scan_mode[qmode]['x_slow']
+        YY = self.scan_mode[qmode]['y_slow']
+        self.analyzer_hw.settings[XX] = x
+        self.analyzer_hw.settings[YY] = y
     
     def move_position_slow(self, x,y, dx, dy):
-        self.analyzer_hw.settings['quad_X1'] = x
-        self.analyzer_hw.settings['quad_Y1'] = y
+        qmode = self.settings['quad_scan_mode']
+        XX = self.scan_mode[qmode]['x_slow']
+        YY = self.scan_mode[qmode]['y_slow']
+        self.analyzer_hw.settings[XX] = x
+        self.analyzer_hw.settings[YY] = y
         
     def move_position_fast(self, x,y, dx, dy):
-        self.analyzer_hw.analyzer.write_quad('X1',x)
-        self.analyzer_hw.analyzer.write_quad('Y1',y)
+        qmode = self.settings['quad_scan_mode']
+        XX = self.scan_mode[qmode]['x_fast']
+        YY = self.scan_mode[qmode]['y_fast']
+        self.analyzer_hw.analyzer.write_quad(XX,x)
+        self.analyzer_hw.analyzer.write_quad(YY,y)
         #self.move_position_slow(x, y, dx, dy)
         
 
@@ -114,15 +130,21 @@ class AugerQuadSlowScan(BaseRaster2DSlowScan):
         
     def update_LUT(self):
         self.hist_lut.imageChanged(autoLevel=False)
-        self.hist_lut.setLevels(self.p_min, self.p_max)
+        if hasattr(self, 'p_min'):
+            self.hist_lut.setLevels(self.p_min, self.p_max)
         
     def post_scan_cleanup(self):
         self.analyzer_hw.settings['multiplier'] = False
         
+            #filter image before finding max
         A = self.display_image_map
+        B = ndimage.gaussian_filter(A[0,:,:], sigma=2)
+        self.p_max = np.amax(B)
+        self.p_min = np.amin(B)
+        self.display_image_map[0,:,:] = B
         k,j,i = np.unravel_index(A.argmax(), A.shape)
         
         print("found quad opt peak at ", k,j,i, A[k,j,i], self.h_array[i], self.v_array[j])
-        
-        self.analyzer_hw.settings['quad_X1'] = self.h_array[i] 
-        self.analyzer_hw.settings['quad_Y1'] = self.v_array[j]         
+        self.move_position_slow(self.h_array[i],self.v_array[j],0,0)
+
+   
