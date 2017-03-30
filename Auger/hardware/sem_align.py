b1626e8d7355f35b174cef29a3aab56893dd3467
Edward Barnard <esbarnard@lbl.gov>
2017-03-29 17:19:03 -0700

    sem_align: add crosshair PointLQROI, beamshift control, virtual focus joystick

--- Auger/hardware/sem_align.py
+++ Auger/hardware/sem_align.py
@@ -2,12 +2,15 @@ from ScopeFoundry import Measurement
 import pyqtgraph as pg
 import pyqtgraph.dockarea as dockarea
 import time
+from qtpy import QtWidgets
+import pygame.event
 
 class SEMAlignMeasure(Measurement):
     
     name = 'sem_align'
-    
+   
     def setup(self):
+        self.sem = self.app.hardware['sem_remcon']
         
         self.ui = self.dockarea = dockarea.DockArea()
         
@@ -15,51 +18,56 @@ class SEMAlignMeasure(Measurement):
         
         self.dockarea.addDock(name='SEM Settings', position='left', widget=self.sem_controls)
         
-            #stig=============
+        #stig=============
         self.stig_plot = pg.PlotWidget()
         self.stig_plot.setAspectLocked(1.0)
         
-        #self.stig_boundary = pg.RectROI([0,0],[100,100], centered=True, movable=False)
         a = 100
+        # Boundary
         self.stig_plot.plot([-a, a, a, -a, -a], [-a,-a, a, a, -a])
         
-        self.stig_pt_roi = pg.RectROI([0,0], size=pg.Point(1,1), movable=True)
-
-        
+        self.stig_pt_roi = PointLQROI(pos=[0,0], size=(a,a), pen=(0,9),  movable=True)
         self.stig_plot.addItem(self.stig_pt_roi)
+        self.stig_pt_roi.connect_lq(self.sem.settings.stig_xy)
         
-        #self.stig_pt_roi.removeHandle(0)
-        self.stig_pt_roi.handleSize = 25
-        #self.stig_pt_roi.addTranslateHandle((0,0))
-        
-        
+        self.stig_pt_roi.removeHandle(0)
+                
         self.dockarea.addDock(name='Stigmation', position='right', widget=self.stig_plot)
         
         self.stig_plot.showGrid(x=True, y=True, alpha=1.0)
         
-        self.stig_pt_roi.sigRegionChanged[object].connect(self.on_update_stig_roi)
         
-        self.stig_roi_moving=False
-        def _start(x):
-            print("start moving")
-            self.stig_roi_moving=True
-        def _finish(x):
-            print("finish moving")
-            self.stig_roi_moving=False
-        self.stig_pt_roi.sigRegionChangeStarted[object].connect(_start)
-        self.stig_pt_roi.sigRegionChangeFinished[object].connect(_finish)
-         
-        self.sem = self.app.hardware['sem_remcon']
+        #beam shift=============
+        self.beamshift_plot = pg.PlotWidget()
+        self.beamshift_plot.setAspectLocked(1.0)
         
-        self.sem.settings.stig_xy.add_listener(self.on_update_stig_xy)
+        a = 100
+        # Boundary
+        self.beamshift_plot.plot([-a, a, a, -a, -a], [-a,-a, a, a, -a])
         
-        self.last_stig_update = time.time()
-        self.stig_update_interval = 0.050
-    
-            #focus==============
+        self.beamshift_roi = PointLQROI(pos=[0,0], size=(a,a), pen=(0,9),  movable=True)
+        self.beamshift_plot.addItem(self.beamshift_roi)
+        self.beamshift_roi.connect_lq(self.sem.settings.beamshift_xy)
+        
+        self.beamshift_roi.removeHandle(0)
+                
+        self.dockarea.addDock(name='Beam Shift', position='right', widget=self.beamshift_plot)
+        
+        self.beamshift_plot.showGrid(x=True, y=True, alpha=1.0)
+            
+        #focus==============
+        self.wd_widget = QtWidgets.QWidget()
+        self.wd_widget.setLayout(QtWidgets.QVBoxLayout())
+        self.dockarea.addDock(name='Focus', position='left', widget=self.wd_widget)
+        
+        self.wd_joystick = pg.JoystickButton()
+        self.wd_joystick.sigStateChanged.connect(self.on_wd_joystick)
+        self.wd_widget.layout().addWidget(self.wd_joystick, stretch=1)
+        
         self.wd_plot = pg.PlotWidget()
         self.wd_plot.invertY(True)
-        self.dockarea.addDock(name='Focus', position='left', widget=self.wd_plot)
+        self.wd_widget.layout().addWidget(self.wd_plot, stretch=1)
+        
         
         self.wd_line = pg.InfiniteLine(1, angle=0, pen=pg.mkPen((0,9), width=4), movable=True)
         self.wd_plot.addItem(self.wd_line)
@@ -69,29 +77,66 @@ class SEMAlignMeasure(Measurement):
         self.wd_plot.addItem(self.wd_lim1)   
         
         self.wd_line.sigDragged.connect(self.on_update_wd_line)     
+        self.sem.settings.WD.add_listener(self.wd_line.setPos, float)
         
-    
+            
     def on_update_wd_line(self, line=None):
         self.sem.settings['WD'] = self.wd_line.getYPos()
 
-    def on_update_stig_roi(self, roi=None):
+    def on_wd_joystick(self, jb, state):
+        dx, dy = state
+        self.sem.settings['WD'] += -dy*1e-1
+        print(jb, state)
+        #print(self.wd_joystick.getState())
+
         
-        if self.stig_roi_moving:
-            if roi is None:
-                roi = self.stig_pt_roi
-            
-            roi_state = roi.saveState()
+
+
+
+class PointLQROI(pg.CrosshairROI):
+    """
+    An ROI that can track an (x,y) style array logged quantity
+    """
+    
+    def __init__(self, pos=None, size=None, **kargs):
+        pg.CrosshairROI.__init__(self, pos=pos, size=size, **kargs)
+        
+        self.lq = None
+        self.roi_drag=False
+
+        self.sigRegionChangeStarted[object].connect(self._start_drag)
+        self.sigRegionChangeFinished[object].connect(self._finish_drag)
+        self.sigRegionChanged[object].connect(self.on_update_roi)
+        
+        #self.removeHandle(0)
+        
+    def connect_lq(self, lq):
+        self.lq = lq
+        lq.add_listener(self.on_update_lq)
+        
+
+    def _start_drag(self, x):
+        print("start moving")
+        self.roi_drag=True
+        
+    def _finish_drag(self, x):
+        print("finish moving")
+        self.roi_drag=False
+        
+    def on_update_roi(self, _=None):
+        if self.roi_drag:
+            roi_state = self.saveState()
             x, y = roi_state['pos']
-            t = time.time()
-            if t - self.last_stig_update > self.stig_update_interval:
-                #print("time elapsed", t - self.last_stig_update , 'going')
-                self.sem.settings['stig_xy'] = [x,y]
-                self.last_stig_update = t
-            
-    def on_update_stig_xy(self):
-        if not self.stig_roi_moving:
-            self.stig_pt_roi.setPos(*self.sem.settings['stig_xy'])
-        
+
+            if self.lq:
+                self.lq.update_value([x,y])
+
+    def on_update_lq(self):
+        if not self.roi_drag:
+            x, y = self.lq.val
+            self.setPos(x,y)
+
+
 class PointROI(pg.ROI):
     
     def __init__(self, pos, **args):
