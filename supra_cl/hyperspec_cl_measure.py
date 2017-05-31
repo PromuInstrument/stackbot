739289b2a3645f4ec84a6519d09c2e1dc7ab870d
Edward Barnard <esbarnard@lbl.gov>
2017-05-30 21:00:44 -0700

    hyperspec_cl: Fix H5 data saving

--- supra_cl/hyperspec_cl_measure.py
+++ supra_cl/hyperspec_cl_measure.py
@@ -1,5 +1,6 @@
 from ScopeFoundryHW.sync_raster_daq import SyncRasterScan
 import numpy as np
+import time
 
 class HyperSpecCLMeasure(SyncRasterScan):
     
@@ -51,34 +52,45 @@ class HyperSpecCLMeasure(SyncRasterScan):
         
         
     
-        Ny, Nx = ccd.settings['readout_shape']
+        ccd_Ny, ccd_Nx = ccd.settings['readout_shape']
     
-        assert Ny == 1
+        assert ccd_Ny == 1
     
         # create data buffer
-        self.spec_buffer = np.zeros(shape=(self.Npixels, Nx), dtype=np.int32) # buffer for spectra (for one spatial frame)
-        self.spec_map = np.zeros( shape=self.scan_shape + (Nx,), dtype=np.int32) # spatial hyperspec data cube
-          
+        self.spec_buffer = np.zeros(shape=(self.Npixels, ccd_Nx), dtype=np.int32) # buffer for spectra (for one spatial frame)
+        self.spec_map = np.zeros( shape=self.scan_shape + (ccd_Nx,), dtype=np.int32) # spatial hyperspec data cube
+        
+        Nk, Nj, Ni = self.scan_shape
+        
         self.andor_ccd_pixel_i = 0
         self.andor_ccd_total_i = 0
         if self.settings['save_h5']:
-            self.spec_map_h5 = self.create_h5_framed_dataset('spec_map', self.spec_map)
+            self.spec_map_h5 = self.create_h5_framed_dataset('spec_map', self.spec_map, compression=None, chunks=(1, 1, 1, Ni, ccd_Nx))
             #self.h5_m['wls'] = 
 
         # start ccd camera, wait for trigger
         ccd.ccd_dev.start_acquisition()
+        
+        #self.time_of_last_andor_get = time.time()
     
     def handle_new_data(self):
         """ Called during measurement thread wait loop"""
         SyncRasterScan.handle_new_data(self)
         
-        
+        # new frame
+        if self.andor_ccd_pixel_i == 0:
+            frame_num = (self.andor_ccd_total_i // self.Npixels) - 1
+            if self.settings['save_h5']:
+                self.extend_h5_framed_dataset(self.spec_map_h5, frame_num)
+                #self.spec_map_h5[frame_num,:,:,:,:] = self.spec_map
+
+        #print("handle andor")
         ccd_dev = self.andor_ccd.ccd_dev
 #        print("get_number_new_images", ccd_dev.get_number_new_images()
-        print("get_total_number_images_acquired", ccd_dev.get_total_number_images_acquired())
+        #print("get_total_number_images_acquired", ccd_dev.get_total_number_images_acquired())
         if ccd_dev.get_total_number_images_acquired() > 0:
-            print("get_number_available_images", ccd_dev.get_number_available_images())
-            print("get_number_new_images", ccd_dev.get_number_new_images())
+            #print("get_number_available_images", ccd_dev.get_number_available_images())
+            #print("get_number_new_images", ccd_dev.get_number_new_images())
             
             #first, last = ccd_dev.get_number_new_images()
             
@@ -86,30 +98,41 @@ class HyperSpecCLMeasure(SyncRasterScan):
             #print("get_images", validfirst, validlast)
 
             while 1:
+                if self.interrupt_measurement_called:
+                    break
                 # grab the next ccd image, place it into buffer
+                t0 = time.time()
                 arr = ccd_dev.get_oldest_image(self.spec_buffer[self.andor_ccd_pixel_i,:])
                 if arr is None:
                     break
                 
                 # copy data to image shaped map
                 i = self.andor_ccd_pixel_i
-                x = self.scan_index_array[i,:]
-                self.spec_map[x[0], x[1], x[2],:] = arr
+                #x = self.scan_index_array[i,:]
+                #self.spec_map[x[0], x[1], x[2],:] = arr
+                kk, jj, ii = self.scan_index_array[i,:]
+                self.spec_map[kk,jj,ii,:] = arr
+                frame_num = (self.andor_ccd_total_i // self.Npixels)
+                if self.settings['save_h5']:
+                    self.spec_map_h5[frame_num, kk,jj,ii,:] = arr
 
-                
-                # new frame
-                if self.andor_ccd_pixel_i == 0:
-                    frame_num = (self.andor_ccd_total_i // self.Npixels) - 1
-                    if self.settings['save_h5']:
-                        self.extend_h5_framed_dataset(self.spec_map_h5, frame_num)
-                        self.spec_map_h5[frame_num,:,:,:,:] = self.spec_map
                 
                 self.andor_ccd_pixel_i += 1
                 self.andor_ccd_total_i += 1
                 self.andor_ccd_pixel_i %= self.Npixels
                 
-                print('oldest image', arr.shape, np.max(arr))
-            
+                
+                #print('oldest image', arr.shape, np.max(arr), "%f" % (time.time()-t0) )
+
+#         if (time.time() - self.time_of_last_andor_get) > 1:
+#             self.time_of_last_andor_get = time.time()
+# 
+#             frame_num = (self.andor_ccd_total_i // self.Npixels)
+#             print(frame_num)
+#             self.spec_map_h5[frame_num, :,:,:,:] = self.spec_map                
+# 
+#             print("save h5 chunk", time.time() - self.time_of_last_andor_get)
+#             
         
     def post_scan_cleanup(self):
         self.andor_ccd.interrupt_acquisition()
