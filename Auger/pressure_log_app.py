31b39a862bd8ae4e2726e7e0b90b622299857148
Edward Barnard <esbarnard@lbl.gov>
2017-10-11 12:48:55 -0700

    Work to make pressure_log and independent app. Now logs turbo status as well as pressure, uses C modules not x-series board. HW component for turbo serial interface. Saves to hdf5 in resizable arrays. Still need to include temperature.

--- Auger/pressure_log_app.py
+++ Auger/pressure_log_app.py
@@ -12,24 +12,24 @@ logging.getLogger('traitlets').setLevel(logging.WARNING)
 
 class PressureLogApp(BaseMicroscopeApp):
     '''
-    stand alone app for pressure monitoring, will later use C-9215 for logging
+    stand alone app for pressure monitoring, now uses 9215A USB module for logging
     independent of x-series board
     '''
     name = 'PressureLogApp'
     
     def setup(self):
                  
-        # SEM Hardware Components
-
+        # Hardware Components
+        from Auger.hardware.pfeiffer_turbo import TC110TurboHW
+        self.add_hardware(TC110TurboHW(self))
 
         # Measurements
-                
         from Auger.measurement.auger_pressure_history import AugerPressureHistory
         self.add_measurement_component(AugerPressureHistory(self))
 
+        # Load default settings
         self.settings_load_ini('pressure_log_app_settings.ini')
 
-        self.ui.show()
         
         
 if __name__ == '__main__':
