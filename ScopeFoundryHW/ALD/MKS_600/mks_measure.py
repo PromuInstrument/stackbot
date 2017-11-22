b69a9891286bad26d63aa0c157287561a48f05a6
Alan Buckley <alanbuckley@lbl.gov>
2017-11-21 16:39:05 -0800

    [ALD] MKS 600 measurement module. Initial commit.

---
+++ ScopeFoundryHW/ALD/MKS_600/mks_measure.py
@@ -0,0 +1,36 @@
+'''
+Created on Nov 21, 2017
+
+@author: Alan Buckley <alanbuckley@berkeley.edu>
+                      <alanbuckley@lbl.gov>
+'''
+
+from ScopeFoundry import Measurement
+from PyQt5 import QtWidgets
+import time 
+
+class MKS_Measure(Measurement):
+    
+    name = "mks_measure"
+    
+    def __init__(self, app):
+        Measurement.__init__(self, app)
+        
+    def setup(self):
+        
+        self.ui = QtWidgets.QWidget()
+        self.layout = QtWidgets.QVBoxLayout()
+        self.ui.setLayout(self.layout)
+    
+        self.mks = self.app.hardware.mks_hw
+        
+    def run(self):
+        dt=0.1
+        while not self.interrupt_measurement_called:
+            self.mks.settings.valve_position.read_from_hardware()
+            time.sleep(dt)
+            self.mks.settings.pressure.read_from_hardware()
+            time.sleep(dt)
+            self.mks.settings.units.read_from_hardware()
+            time.sleep(0.25)
+    
