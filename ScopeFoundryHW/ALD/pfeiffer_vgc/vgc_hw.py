c315546475f6c99d9230cc89ec4ecd327dd93c7c
Alan Buckley <alanbuckley@lbl.gov>
2017-11-21 16:36:35 -0800

    [ALD] Pfeiffer VGC hardware module. Fixed issues related to the way logged quantities and their respective function maps were defined.

--- ScopeFoundryHW/ALD/pfeiffer_vgc/vgc_hw.py
+++ ScopeFoundryHW/ALD/pfeiffer_vgc/vgc_hw.py
@@ -13,25 +13,29 @@ class VGC_Hardware(HardwareComponent):
     name = "vgc_hw"
     
     def setup(self):
-        self.port = self.settings.New(name="port", initial="COM3", dtype=str, ro=False)
-        self.ch1_pressure = self.settings.New(name="ch1_pressure", initial=0.0, dtype=float, ro=True)
-        self.ch1_pressure_units = self.settings.New(name="ch1_units", initial="mbar", dtype=str, ro=True)
+        self.settings.New(name="port", initial="COM3", dtype=str, ro=False)
+        self.settings.New(name="ch1_pressure", initial=0.0, dtype=float, ro=True)
+        self.settings.New(name="ch1_units", initial="mbar", dtype=str, ro=True)
+        self.settings.New(name="ch1_sensor_type", initial="None", dtype=str,  ro=True)
+        
         self.vgc = None
         
     def connect(self):
-        self.vgc = vgc_interface(port=self.port.val, debug=self.settings['debug_mode'])
+        self.vgc = vgc_interface(port=self.settings.port.val, debug=self.settings['debug_mode'])
 
         self.settings.ch1_pressure.connect_to_hardware(read_func=getattr(self,'read_ch1_pressure'))
-        self.settings.ch1_pressure_units.connect_to_hardware(read_func=getattr(self, 'read_ch1_units'))
+        
+        self.settings.ch1_sensor_type.connect_to_hardware(read_func=getattr(self, 'read_ch1_sensor_type'))
+        
         
         
     def read_ch1_pressure(self):
         sensor = 1
         return self.vgc.read_sensor(sensor)
     
-    def read_ch1_units(self):
-        channel = 1
-        return self.vgc.sensor_type(channel)
+    def read_ch1_sensor_type(self):
+        channel = 0
+        return self.vgc.sensor_type()[channel]
     
     def disconnect(self):
         self.settings.disconnect_all_from_hardware()
