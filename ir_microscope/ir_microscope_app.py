126821a040597d937e398218db32a065796ac6ee
Benedikt Ursprung <bursprung@lbl.gov>
2017-11-30 14:51:23 -0800

    ir_microscope as of 20171130

--- ir_microscope/ir_microscope_app.py
+++ ir_microscope/ir_microscope_app.py
@@ -2,6 +2,12 @@ from __future__ import division, print_function
 from ScopeFoundry import BaseMicroscopeApp
 from ScopeFoundry.helper_funcs import sibling_path, load_qt_ui_file
 import logging
+from ScopeFoundryHW import attocube_ecc100
+import ScopeFoundryHW
+from ir_microscope.measurements import apd_scan, hyperspectral_scan,\
+    picoharp_attocube_slow_scans
+from ir_microscope import measurements
+
 
 logging.basicConfig(level='DEBUG')#, filename='m3_log.txt')
 #logging.getLogger('').setLevel(logging.WARNING)
@@ -17,18 +23,182 @@ class IRMicroscopeApp(BaseMicroscopeApp):
     
     def setup(self):
         
-        #self.add_quickbar(load_qt_ui_file(sibling_path(__file__, 'trpl_quick_access.ui')))
+        self.add_quickbar(load_qt_ui_file(sibling_path(__file__, 'ir_quick_access.ui')))
         
         print("Adding Hardware Components")
+        
         import ScopeFoundryHW.picoharp as ph
         self.add_hardware(ph.PicoHarpHW(self))
 
+        from ScopeFoundryHW.winspec_remote import WinSpecRemoteClientHW
+        self.add_hardware_component(WinSpecRemoteClientHW(self))  
+                
+        from ScopeFoundryHW.tenma_power.tenma_hw import TenmaHW
+        self.add_hardware(TenmaHW(self))
+
+
+        #from ScopeFoundryHW.apd_counter import APDCounterHW
+        #self.add_hardware(APDCounterHW(self))
+        
+        #from ScopeFoundryHW.acton_spec.acton_spec import ActonSpectrometerHW
+        #self.add_hardware(ActonSpectrometerHW(self))
+
+
+        from ScopeFoundryHW.powerwheel_arduino import PowerWheelArduinoHW
+        self.power_wheel = self.add_hardware_component(PowerWheelArduinoHW(self))    
+        
+        from ScopeFoundryHW.thorlabs_powermeter import ThorlabsPowerMeterHW
+        self.add_hardware_component(ThorlabsPowerMeterHW(self))
+
+        from ScopeFoundryHW.dli_powerswitch import DLIPowerSwitchHW
+        self.add_hardware(DLIPowerSwitchHW(self))
+
+        from ScopeFoundryHW.pololu_servo.pololu_hw import PololuHW
+        self.add_hardware(PololuHW(self))        
+        
+        from ScopeFoundryHW.attocube_ecc100.attocube_xyz_hw import AttoCubeXYZStageHW
+        self.add_hardware(AttoCubeXYZStageHW(self))                        
+              
+        from ScopeFoundryHW.powermate.powermate_hw import PowermateHWSimple
+        self.add_hardware(PowermateHWSimple)
+        
+        from ScopeFoundryHW.thorlabs_motorized_filter_flipper.thorlabsMFF_hardware import ThorlabsMFFHW
+        self.add_hardware_component(ThorlabsMFFHW(self))        
+        
+                        
+        print("Adding Measurement Components")
+
         self.add_measurement(ph.PicoHarpChannelOptimizer(self))
         self.add_measurement(ph.PicoHarpHistogramMeasure(self))
+        self.add_measurement(picoharp_attocube_slow_scans.Picoharp_AttoCube_2DSlowScan(self))
+        
+        from ScopeFoundryHW.winspec_remote import WinSpecRemoteReadoutMeasure
+        self.add_measurement_component(WinSpecRemoteReadoutMeasure(self))
+        self.add_measurement(hyperspectral_scan.M4Hyperspectral2DScan(self))
+        
+        #self.add_measurement(apd_scan.M4APDScanPhMeasure(self))
+        
+        from confocal_measure.power_scan import PowerScanMeasure
+        self.add_measurement_component(PowerScanMeasure(self))
+        
+        from ScopeFoundryHW.thorlabs_powermeter import PowerMeterOptimizerMeasure
+        self.add_measurement_component(PowerMeterOptimizerMeasure(self))    
+ 
+        from ScopeFoundryHW.attocube_ecc100.attocube_stage_control import AttoCubeStageControlMeasure
+        self.add_measurement(AttoCubeStageControlMeasure(self))
+
+        from ScopeFoundryHW.powermate.powermate_measure import PowermateMeasureSimple
+        self.add_measurement(PowermateMeasureSimple)
+        
+        ####### Quickbar connections #################################
+        
+        Q = self.quickbar
+        
+        # Powermate
+        pm_measure = self.measurements['powermate_measure'] 
+        pm_measure.settings.pm_0_data_handler_selector.connect_to_widget(Q.powermate_0_comboBox)
+        pm_measure.settings.pm_1_data_handler_selector.connect_to_widget(Q.powermate_1_comboBox)
+
+        # LED
+        tenma = self.hardware['tenma_powersupply']
+        Q.tenma_power_on_pushButton.clicked.connect(lambda: tenma.write_both(V=3.0,I=0.1,impose_connection=True))
+        Q.tenma_power_off_pushButton.clicked.connect(tenma.zero_both)
+        
+        tenma.settings.actual_current.connect_to_widget(Q.tenma_power_current_doubleSpinBox)
+        Q.tenma_power_current_lineEdit.returnPressed.connect(tenma.settings.set_current.update_value)
+        Q.tenma_power_current_lineEdit.returnPressed.connect(lambda: Q.tenma_power_current_lineEdit.setText(""))
+
+        tenma.settings.actual_voltage.connect_to_widget(Q.tenma_power_voltage_doubleSpinBox)
+        Q.tenma_power_voltage_lineEdit.returnPressed.connect(tenma.settings.set_voltage.update_value)
+        Q.tenma_power_voltage_lineEdit.returnPressed.connect(lambda: Q.tenma_power_voltage_lineEdit.setText(""))
+        
+        # Atto Cube
+        stage = self.hardware['attocube_xyz_stage']
+
+        stage.settings.x_position.connect_to_widget(Q.cx_doubleSpinBox)
+        Q.x_set_lineEdit.returnPressed.connect(stage.settings.x_target_position.update_value)
+        Q.x_set_lineEdit.returnPressed.connect(lambda: Q.x_set_lineEdit.setText(""))
+
+        stage.settings.y_position.connect_to_widget(Q.cy_doubleSpinBox)
+        Q.y_set_lineEdit.returnPressed.connect(stage.settings.y_target_position.update_value)
+        Q.y_set_lineEdit.returnPressed.connect(lambda: Q.y_set_lineEdit.setText(""))
+
+        stage.settings.z_position.connect_to_widget(Q.cz_doubleSpinBox)
+        Q.z_set_lineEdit.returnPressed.connect(stage.settings.z_target_position.update_value)
+        Q.z_set_lineEdit.returnPressed.connect(lambda: Q.z_set_lineEdit.setText(""))
+
+        self.measurements['AttoCubeStageControlMeasure'].settings.activation.connect_to_widget(Q.stage_live_update_checkBox)
+        #stage.settings.move_speed.connect_to_widget(Q.nanodrive_move_slow_doubleSpinBox)   
+        
+        #connect events
+        #apd = self.hardware['apd_counter']
+        #apd.settings.int_time.connect_to_widget(Q.apd_counter_int_doubleSpinBox)
+        #apd.settings.apd_count_rate.updated_text_value.connect(
+                                           #Q.apd_counter_output_lineEdit.setText)
         
 
+        #apd_opt = self.measurements['apd_optimizer']
+        #apd.settings
+        #apd_opt.settings.activation.connect_to_widget(Q.apd_optimize_startstop_checkBox)
+        #self.measurement_state_changed[bool].connect(self.gui.ui.apd_optimize_startstop_checkBox.setChecked)         
+        
+        # power meter
+        pm = self.hardware['thorlabs_powermeter']
+        pm.settings.wavelength.connect_to_widget(Q.power_meter_wl_doubleSpinBox)
+        pm.settings.power.connect_to_widget(Q.power_meter_power_label)
+        
+        pm_opt = self.measurements['powermeter_optimizer']
+        pm_opt.settings.activation.connect_to_widget(Q.power_meter_acquire_cont_checkBox)
+        
+        # Power Wheel
+        pw = self.hardware['power_wheel_arduino']
+        pw.settings.encoder_pos.connect_to_widget(Q.power_wheel_encoder_pos_doubleSpinBox)
+        pw.settings.move_steps.connect_to_widget(Q.powerwheel_move_steps_doubleSpinBox)
+        Q.powerwheel_move_fwd_pushButton.clicked.connect(pw.move_fwd)
+        Q.powerwheel_move_bkwd_pushButton.clicked.connect(pw.move_bkwd)
+
+
+        # Thorlabs Flip Mirror
+        MFF = self.hardware['thorlabs_MFF']
+        MFF.settings.pos.connect_to_widget(Q.thorlabs_MFF_comboBox)
+        
+        # shutter
+        shutter = self.hardware['pololu_servo_hw']
+        shutter.settings.servo2_toggle.connect_to_widget(Q.shutter_open_checkBox)
+               
+        
+
+        """
+        # Andor CCD
+        andor = self.hardware['andor_ccd']
+        andor.settings.exposure_time.connect_to_widget(Q.andor_ccd_int_time_doubleSpinBox)
+        andor.settings.em_gain.connect_to_widget(Q.andor_ccd_emgain_doubleSpinBox)
+        andor.settings.temperature.connect_to_widget(Q.andor_ccd_temp_doubleSpinBox)
+        andor.settings.ccd_status.connect_to_widget(Q.andor_ccd_status_label)
+        andor.settings.shutter_open.connect_to_widget(Q.andor_ccd_shutter_open_checkBox)  
+        
+        # Spectrometer
+        aspec = self.hardware['acton_spectrometer']
+        aspec.settings.center_wl.connect_to_widget(Q.acton_spec_center_wl_doubleSpinBox)
+        aspec.settings.exit_mirror.connect_to_widget(Q.acton_spec_exitmirror_comboBox)
+        aspec.settings.grating_name.connect_to_widget(Q.acton_spec_grating_lineEdit)                
+        
+        
+        #connect events
+        apd = self.hardware['apd_counter']
+        apd.settings.int_time.connect_to_widget(Q.apd_counter_int_doubleSpinBox)
+        apd.settings.apd_count_rate.updated_text_value.connect(
+                                           Q.apd_counter_output_lineEdit.setText)
+        
+
+        #apd_opt = self.measurements['apd_optimizer']
+        #apd.settings
+        #apd_opt.settings.activation.connect_to_widget(Q.apd_optimize_startstop_checkBox)
+        #self.measurement_state_changed[bool].connect(self.gui.ui.apd_optimize_startstop_checkBox.setChecked)      
+        """
+        
         ##########
-        self.settings_load_ini('trpl_defaults.ini')
+        self.settings_load_ini('ir_microscope_defaults.ini')
 
 if __name__ == '__main__':
     import sys
---
+++ ir_microscope/measurements/picoharp_attocube_slow_scans.py
@@ -0,0 +1,228 @@
+from __future__ import print_function, absolute_import, division
+from ScopeFoundryHW.mcl_stage import MCLStage2DSlowScan
+import ScopeFoundry
+from ScopeFoundry.helper_funcs import sibling_path
+
+import numpy as np
+import time
+import pyqtgraph as pg
+from ScopeFoundryHW.attocube_ecc100.attocube_slowscan import AttoCube2DSlowScan
+
+class Picoharp_AttoCube_2DSlowScan(AttoCube2DSlowScan):
+    
+    name = 'trpl_scan'
+    def setup(self):
+        AttoCube2DSlowScan.setup(self)
+        self.ph_hw = self.app.hardware['picoharp']
+        
+        dui_filename = sibling_path(__file__,"picoharp_hist_measure_details.ui")
+        self.dui = self.set_details_widget(dui_filename)
+        
+        ph_hw = self.ph_hw
+
+        S = self.settings
+        S.New('use_calc_hist_chans', dtype=bool, initial=True)
+        
+
+        S.use_calc_hist_chans.connect_to_widget(self.dui.use_calc_hist_chans_checkBox)        
+        ph_hw.settings.Tacq.connect_to_widget(self.dui.picoharp_tacq_doubleSpinBox)
+        ph_hw.settings.Binning.connect_to_widget(self.dui.Binning_comboBox)
+        ph_hw.settings.count_rate0.connect_to_widget(self.dui.ch0_doubleSpinBox)
+        ph_hw.settings.count_rate1.connect_to_widget(self.dui.ch1_doubleSpinBox)
+        ph_hw.settings.histogram_channels.connect_to_widget(self.dui.histogram_channels_doubleSpinBox)
+        
+    
+    def pre_scan_setup(self):
+        
+        self.ph  = self.ph_hw.picoharp
+        
+        self.sleep_time = min((max(0.1*self.ph.Tacq*1e-3, 0.010), 0.100))
+        if self.settings['use_calc_hist_chans']:
+            self.ph_hw.settings['histogram_channels'] = self.ph_hw.calc_num_hist_chans() 
+                    
+        self.num_hist_chans=self.ph_hw.settings['histogram_channels']
+        self.data_slice = slice(0,self.num_hist_chans)
+
+        time_trace_map_shape = self.scan_shape + (self.num_hist_chans,)
+
+        self.time_trace_map_h5 = self.h5_meas_group.create_dataset('time_trace_map', 
+                                                                   shape=time_trace_map_shape,
+                                                                   dtype=float, 
+                                                                   compression='gzip')
+        
+        
+        self.time_array = self.ph.time_array*1e-3
+        self.h5_meas_group['time_array'] = self.time_array[self.data_slice]
+        self.elapsed_time_h5 = self.h5_meas_group['elapsed_time'] = np.zeros(self.scan_shape, dtype=float)
+        
+        #self.app.settings_auto_save()
+        
+
+        # pyqt graph
+        self.initial_scan_setup_plotting = True
+
+    def post_scan_cleanup(self):
+        # close shutter 
+        #self.gui.shutter_servo_hc.shutter_open.update_value(False)
+        pass
+    
+    def collect_pixel(self, pixel_num, k, j, i):
+        
+        # collect data
+        print(self.name, 'collect_pixel', pixel_num, k, j, i)
+        #t0 = time.time()
+
+        #hist_data, elapsed_time = self.read_picoharp_histogram()
+        
+        ph = self.ph_hw.picoharp
+        ph.start_histogram()
+        while not ph.check_done_scanning():
+            self.ph_hw.settings.count_rate0.read_from_hardware()
+            self.ph_hw.settings.count_rate1.read_from_hardware()
+            if self.ph_hw.settings['Tacq'] > 0.2:
+                ph.read_histogram_data()
+            time.sleep(0.005) #self.sleep_time)  
+        ph.stop_histogram()
+        #ta = time.time()
+        ph.read_histogram_data()
+
+        hist_data = ph.histogram_data[self.data_slice]
+        elapsed_time = ph.read_elapsed_meas_time()
+
+        self.time_trace_map_h5[k,j,i, :] = hist_data
+        
+        self.elapsed_time_h5[k,j,i] = elapsed_time
+
+        # display count-rate
+        self.display_image_map[k,j,i] = hist_data.sum() * 1.0/elapsed_time
+        
+        import datetime
+        print('pixel',  datetime.timedelta(seconds=(self.Npixels - pixel_num)*elapsed_time*1e-3), 'left')
+        
+        print( 'pixel done' )
+    
+    def read_picoharp_histogram(self):
+
+        ph = self.ph_hw.picoharp
+
+        ph.start_histogram()
+
+        while not ph.check_done_scanning():
+            if self.ph_hw.settings['Tacq'] > 200:
+                ph.read_histogram_data()
+            time.sleep(0.005) #self.sleep_time)  
+        ph.stop_histogram()
+        #ta = time.time()
+        ph.read_histogram_data()
+
+        return ph.histogram_data, ph.read_elapsed_meas_time()
+    
+    def read_picoharp_timearray(self):
+        return self.ph.time_array()
+
+        
+    def update_display(self):
+        AttoCube2DSlowScan.update_display(self)
+        
+        # setup lifetime window
+        if not hasattr(self, 'lifetime_graph_layout'):
+            self.lifetime_graph_layout = pg.GraphicsLayoutWidget()
+            self.lifetime_plot = self.lifetime_graph_layout.addPlot()
+            self.lifetime_plotdata = self.lifetime_plot.plot()
+            self.lifetime_plot.setLogMode(False, True)
+            self.lifetime_plot.enableAutoRange('y',True)
+        
+        if not hasattr(self, 'infline'):
+            self.infline = pg.InfiniteLine(movable=False, angle=90, label='stored_hist_chans', 
+                labelOpts={'position':0.1, 'color': (200,200,100), 'fill': (200,200,200,50), 'movable': True}) 
+            self.lifetime_plot.addItem(self.infline)
+            
+            
+                
+
+        self.lifetime_graph_layout.show()
+        pos_x = self.time_array[self.ph_hw.settings['histogram_channels']]
+        self.lifetime_plot.setRange(xRange=(0,1.2*pos_x))
+        self.lifetime_plot.enableAutoRange('y',True)
+        
+        self.infline.setPos([pos_x,0])
+        
+        self.infline.label.setPosition(0.8)        
+        
+        #kk, jj, ii = self.current_scan_index
+        ph = self.ph_hw.picoharp
+        self.lifetime_plotdata.setData(self.time_array,  1+ph.histogram_data)
+
+'''        
+class Picoharp_AttoCube_3DSlowScan(AttoCube3DStackSlowScan):
+    
+    name = 'Picoharp_AttoCube_3DSlowScan'
+    
+    def pre_scan_setup(self):
+        #hardware 
+        self.ph_hw = self.app.hardware['picoharp']
+        ph = self.ph_hw.picoharp # low level hardware
+        
+        #scan specific setup
+        
+        # create data arrays
+        
+        cr0 = self.ph_hw.settings.count_rate0.read_from_hardware()
+        rep_period_s = 1.0/cr0
+        time_bin_resolution = self.ph_hw.settings['Resolution']*1e-12
+        self.num_hist_chans = int(np.ceil(rep_period_s/time_bin_resolution))
+
+        time_trace_map_shape = self.scan_shape + (self.num_hist_chans,)
+        self.time_trace_map = np.zeros(time_trace_map_shape, dtype=float)
+        
+        
+        self.time_trace_map_h5 = self.create_h5_framed_dataset(name='time_trace_map', single_frame_map=self.time_trace_map)
+        
+        self.time_array = self.h5_meas_group['time_array'] = ph.time_array[0:self.num_hist_chans]*1e-3
+        
+        self.elapsed_time = np.zeros(self.scan_shape, dtype=float)
+        self.elapsed_time_h5 = self.create_h5_framed_dataset('elasped_time', self.elapsed_time)
+        
+        #self.app.settings_auto_save()
+        
+        # pyqt graph
+        #self.initial_scan_setup_plotting = True
+
+        
+    def post_scan_cleanup(self):
+        # close shutter 
+        #self.gui.shutter_servo_hc.shutter_open.update_value(False)
+        pass
+    
+    def collect_pixel(self, pixel_num, frame_i, k, j, i):
+        
+        # collect data
+        print(pixel_num, frame_i, k, j, i)
+        t0 = time.time()
+        
+        hist_data, elapsed_time = Picoharp_MCL_2DSlowScan.read_picoharp_histogram(self)
+
+        # store in arrays
+        self.time_trace_map[k,j,i, :] = hist_data[0:self.num_hist_chans]
+        self.time_trace_map_h5[frame_i,k,j,i, :] = hist_data[0:self.num_hist_chans]
+        
+        self.elapsed_time[k,j,i] = elapsed_time
+        self.elapsed_time_h5[frame_i, k,j,i] = elapsed_time
+
+        # display count-rate
+        self.display_image_map[k,j,i] = hist_data[0:self.num_hist_chans].sum() * 1.0/elapsed_time
+        
+        print( 'pixel done' )
+        
+    def on_new_frame(self, frame_i):
+        AttoCube3DStackSlowScan.on_new_frame(self, frame_i)
+        self.extend_h5_framed_dataset(self.time_trace_map_h5, frame_i)
+        self.extend_h5_framed_dataset(self.elapsed_time_h5, frame_i)
+        
+    def on_end_frame(self, frame_i):
+        AttoCube3DStackSlowScan.on_end_frame(self, frame_i)
+        self.h5_file.flush()
+        
+    def update_display(self):
+        AttoCube3DStackSlowScan.update_display(self)
+    '''
