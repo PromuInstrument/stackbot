dce20aeeae78f663b839905d8037a2fd337caa5e
Alan Buckley <alanbuckley@lbl.gov>
2018-01-26 14:02:20 -0800

    [ALD] Seren RF - Created interface, hardware, and measurement level modules to control Seren RF power supply.

---
+++ ScopeFoundryHW/ALD/Seren/seren_measure.py
@@ -0,0 +1,32 @@
+'''
+Created on Jan 26, 2018
+
+@author: Alan Buckley    <alanbuckley@lbl.gov>
+                        <alanbuckley@berkeley.edu>
+'''
+
+from ScopeFoundry import Measurement
+from PyQt5 import QtWidgets
+import time
+
+class Seren_Measure(Measurement):
+    
+    name = "seren_measure"
+    
+    def __init__(self, app):
+        Measurement.__init__(self, app)
+        
+    def setup(self):
+        self.ui = QtWidgets.QWidget()
+        self.layout = QtWidgets.QVBoxLayout()
+        self.ui.setLayout(self.layout)
+    
+        self.seren = self.app.hardware['seren_hw']
+        
+    def run(self):
+        dt = 0.05
+        while not self.interrupt_measurement_called:
+            self.seren.settings.forward_power.read_from_hardware()
+            time.sleep(dt)
+            self.seren.settings.reflected_power.read_from_hardware()
+            time.sleep(dt)
