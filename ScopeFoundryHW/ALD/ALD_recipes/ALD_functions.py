d90acc9e91418a305d10d455c4bee8e6382ac3c1
Alan Buckley <alanbuckley@lbl.gov>
2018-03-21 20:39:57 -0700

    [ALD] ALD functions routine. Work in progress.

--- ScopeFoundryHW/ALD/ALD_recipes/ALD_functions.py
+++ ScopeFoundryHW/ALD/ALD_recipes/ALD_functions.py
@@ -13,22 +13,41 @@ class ALD_routine(Measurement):
     
     def __init__(self):
         self.relay = self.app.hardware['ald_relay_hw']
+        self.lovebox = self.app.hardware['lovebox']
+        self.mks146 = self.app.hardware['mks_146_hw']
         self.vgc = self.app.hardware['pfeiffer_vgc_hw']
         self.seren = self.app.hardware['seren_hw']
         self.seren.serial_toggle(True)
     
+    
+    
+    
     def precursor_1_dose(self, width=0.8):
         """argument 'width' has units of seconds"""
         relay = 0
-        chamber_pressure = 15 #mtorr 
-        pressure = (1000*self.vgc.read_ch1_pressure())/(101325/76000) #mtorr
-        while not (chamber_pressure-1 < pressure < chamber_pressure+1):
-            pressure = (1000*self.vgc.read_ch1_pressure())/(101325/76000)
-        else: 
-            print("Pulse sending--", "pressure:", pressure, "> chamber:", chamber_pressure)
+        chamber_pressure = 15 #mtorr
+        
+        def pressure_and_flow():
+            measure = self.vgc.read_ch1_pressure()
+            pressure = (1000*measure)/(101325/76000) #mtorr
+            flow = self.mks146.MFC0_read_flow()
+            return pressure, flow
+        
+        pressure, flow = pressure_and_flow()
+        while not (chamber_pressure-1 < pressure < chamber_pressure+1) \
+                and not (100 <= flow <= 200):
+            pressure, flow = pressure_and_flow()
+        else:
+            if self.debug_mode:
+                print("Pulse sending--", "pressure:", pressure, "> chamber:", chamber_pressure)
             self.relay.relay.send_pulse(relay, 1e3*width)
-            break
-    
+
+    def precursor_purge(self, pressure_sp, temp_sp):
+        
+        def read_temp_and_pressure():
+            temp = self.lovebox.lovebox.read_temp()
+#             pressure = self.vgc.
+            return temp
     
     def plasma_dose(self, width, power):
         """Argument 'width' has units of seconds
