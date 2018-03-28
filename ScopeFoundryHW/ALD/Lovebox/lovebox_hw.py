58d96a5d33a3d4b9dfe51e217b4d8963f6fa0c5c
Alan Buckley <alanbuckley@lbl.gov>
2018-03-28 15:52:34 -0700

    [ALD] Lovebox. Removed output 2 and unused modes of operation.

--- ScopeFoundryHW/ALD/Lovebox/lovebox_hw.py
+++ ScopeFoundryHW/ALD/Lovebox/lovebox_hw.py
@@ -12,8 +12,8 @@ from ScopeFoundry import HardwareComponent
 class LoveboxHW(HardwareComponent):
     
     name = 'lovebox'
-    
-    CTRL_METHODS = ("PID", "ON/OFF", "Manual", "PID Program Ctrl")
+    CTRL_METHODS = ("PID", "Manual")
+#     CTRL_METHODS = ("PID", "ON/OFF", "Manual", "PID Program Ctrl")
 #     HEAT_COOL_CTRLS = ("Heating")
 
     def setup(self):
@@ -23,7 +23,7 @@ class LoveboxHW(HardwareComponent):
         self.settings.New(name='pv_temp', initial=0.0, dtype=float, spinbox_decimals=1, ro=True)
         self.settings.New(name='sv_setpoint', initial=0.0, dtype=float, spinbox_decimals=1, ro=False)
         self.settings.New(name='output1', initial=0.0, dtype=float, spinbox_decimals=1, ro=False)
-        self.settings.New(name='output2', initial=0.0, dtype=float, spinbox_decimals=1, ro=False)
+#         self.settings.New(name='output2', initial=0.0, dtype=float, spinbox_decimals=1, ro=False)
         
 #         lq.change_ro(True)
         
@@ -47,9 +47,9 @@ class LoveboxHW(HardwareComponent):
         self.settings.get_lq('output1').connect_to_hardware(
                                                     write_func=self.lovebox.set_output1,
                                                     read_func=self.lovebox.read_output1)
-        self.settings.get_lq('output2').connect_to_hardware(
-                                                    write_func=self.lovebox.set_output2,
-                                                    read_func=self.lovebox.read_output2)
+#         self.settings.get_lq('output2').connect_to_hardware(
+#                                                     write_func=self.lovebox.set_output2,
+#                                                     read_func=self.lovebox.read_output2)
         
         
     def set_control_method(self, control):
@@ -59,6 +59,9 @@ class LoveboxHW(HardwareComponent):
     def set_heat_cool_control(self, control):
         select = self.HEAT_COOL_CTRLS.index(control)
         self.lovebox.set_heat_cool_ctrl(select)
+        
+
+        
     
     def disconnect(self):
         self.settings.disconnect_all_from_hardware()
