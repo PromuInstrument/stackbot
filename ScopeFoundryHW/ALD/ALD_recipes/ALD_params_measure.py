ba5c602f690257947df935503481b87455e4f010
Alan Buckley <alanbuckley@lbl.gov>
2018-04-13 15:46:03 -0700

    [ALD] ALD params. Set up RF time history plot; ready for testing. Added some UI features (WIP)

--- ScopeFoundryHW/ALD/ALD_recipes/ALD_params_measure.py
+++ ScopeFoundryHW/ALD/ALD_recipes/ALD_params_measure.py
@@ -22,6 +22,8 @@ class ALD_params(Measurement):
         self.settings.New('RF_pulse_duration', dtype=int, initial=1)
         self.settings.New('history_length', dtype=int, initial=1000, vmin=1)
         
+        self.setup_buffers_constants()
+        
         self.ui_enabled = True
         if self.ui_enabled:
             self.ui_setup()
@@ -30,6 +32,8 @@ class ALD_params(Measurement):
             self.seren = self.app.hardware.seren_hw
         else:
             print('Connect Seren HW component first.')
+
+        
 
     def ui_setup(self):
         self.ui = QtWidgets.QWidget()
@@ -51,20 +55,37 @@ class ALD_params(Measurement):
         self.rf_plot.showGrid(y=True)
         self.rf_plot.addLegend()
         self.rf_widget.layout().addWidget(self.rf_plot_widget)
+        self.rf_plot_names = ['Forward', 'Reflected']
+        self.rf_plot_lines = []
+        for i in range(self.NUM_CHANS):
+            color = pg.intColor(i)
+            plot_line = self.rf_plot.plot([1], pen=pg.mkPen(color, width=2),
+                                          name = self.rf_plot_names[i])
+            self.rf_plot_lines.append(plot_line)
+        self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        self.rf_plot.addItem(self.vLine)
         
         
         self.recipe_control_widget = QtWidgets.QGroupBox('Recipe Controls')
         self.layout.addWidget(self.recipe_control_widget)
-        self.recipe_control_widget.setLayout(QtWidgets.QHBoxLayout())
+        self.recipe_control_widget.setLayout(QtWidgets.QGridLayout())
         
-        self.recipe_start_button = QtWidgets.QPushButton('Start')
-        self.recipe_stop_button = QtWidgets.QPushButton('Stop')
-        self.recipe_control_widget.layout().addWidget(self.recipe_start_button)
-        self.recipe_control_widget.layout().addWidget(self.recipe_stop_button)
+        self.recipe_start_button = QtWidgets.QPushButton('Start Recipe')
+        self.recipe_stop_button = QtWidgets.QPushButton('Stop Recipe')
+        self.recipe_control_widget.layout().addWidget(self.recipe_start_button, 0, 0)
+        self.recipe_control_widget.layout().addWidget(self.recipe_stop_button, 0, 1)
         self.recipe_start_button.clicked.connect(self.app.measurements['ALD_routine'].start)
         self.recipe_stop_button.clicked.connect(self.app.measurements['ALD_routine'].interrupt)
     
+        self.rf_pulse_label = QtWidgets.QLabel('RF Pulse Duration [s]')
+        self.recipe_control_widget.layout().addWidget(self.rf_pulse_label, 1, 0)
+    
+        self.rf_pulse_field = QtWidgets.QDoubleSpinBox()
+        self.recipe_control_widget.layout().addWidget(self.rf_pulse_field, 1, 1)
+        self.settings.RF_pulse_duration.connect_bidir_to_widget(self.rf_pulse_field)
+    
         
+    
     
     def setup_buffers_constants(self):
         self.HIST_LEN = 500
@@ -90,7 +111,13 @@ class ALD_params(Measurement):
             self.plot_lines[i].setData(
                 self.rf_history[i, :self.index])
     
+    
     def run(self):
         dt = 0.2
         self.HIST_LEN = self.settings['history_length']
-        pass
+        while not self.interrupt_measurement_called:
+            self.seren.settings.forward_power.read_from_hardware()
+            self.seren.settings.reflected_power.read_from_hardware()
+            self.routine()
+            time.sleep(dt)
+            
