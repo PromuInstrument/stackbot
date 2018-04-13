f9c07f52eda9cec3de711a6400a5428c29a9d77a
Alan Buckley <alanbuckley@lbl.gov>
2018-04-12 17:16:45 -0700

    [ALD] ALD App. Autoconnect statements added by Ed Barnard.

--- ScopeFoundryHW/ALD/ALD_app.py
+++ ScopeFoundryHW/ALD/ALD_app.py
@@ -21,47 +21,50 @@ class ALD_App(BaseMicroscopeApp):
     def setup(self):
         
         from ScopeFoundryHW.ALD.ALD_relay.ald_relay_hardware import ALDRelayHW
-        self.add_hardware(ALDRelayHW(self))
+        relay = self.add_hardware(ALDRelayHW(self))
+        relay.settings['connected'] = True
         
         from ScopeFoundryHW.ALD.Lovebox.lovebox_hw import LoveboxHW
-        self.add_hardware(LoveboxHW(self))
+        self.add_hardware(LoveboxHW(self)).settings['connected'] = True
         
         from ScopeFoundryHW.ALD.MKS_146.mks_146_hw import MKS_146_Hardware
-        self.add_hardware(MKS_146_Hardware(self))
+        self.add_hardware(MKS_146_Hardware(self)).settings['connected'] = True
          
         from ScopeFoundryHW.ALD.MKS_600.mks_600_hw import MKS_600_Hardware
-        self.add_hardware(MKS_600_Hardware(self))
+        self.add_hardware(MKS_600_Hardware(self)).settings['connected'] = True
         
         from ScopeFoundryHW.ALD.pfeiffer_vgc.pfeiffer_vgc_hw import Pfeiffer_VGC_Hardware
-        self.add_hardware(Pfeiffer_VGC_Hardware(self))
+        self.add_hardware(Pfeiffer_VGC_Hardware(self)).settings['connected'] = True
           
         from ScopeFoundryHW.ALD.Seren.seren_hw import Seren_HW
-        self.add_hardware(Seren_HW(self))
+        self.add_hardware(Seren_HW(self)).settings['connected'] = True
         
         
         from ScopeFoundryHW.ALD.ALD_relay.ald_relay_measure import ALDRelayMeasure
-        self.add_measurement(ALDRelayMeasure(self))
+        self.add_measurement(ALDRelayMeasure(self)).start()
           
         from ScopeFoundryHW.ALD.Lovebox.lovebox_measure import LoveboxMeasure
-        self.add_measurement(LoveboxMeasure(self))
+        self.add_measurement(LoveboxMeasure(self)).start()
         
         from ScopeFoundryHW.ALD.MKS_146.mks_146_measure import MKS_146_Measure
-        self.add_measurement(MKS_146_Measure(self))
+        self.add_measurement(MKS_146_Measure(self)).start()
         
         from ScopeFoundryHW.ALD.MKS_600.mks_600_measure import MKS_600_Measure
-        self.add_measurement(MKS_600_Measure(self))
+        self.add_measurement(MKS_600_Measure(self)).start()
          
         from ScopeFoundryHW.ALD.pfeiffer_vgc.pfeiffer_vgc_measure import Pfeiffer_VGC_Measure
-        self.add_measurement(Pfeiffer_VGC_Measure(self))
+        self.add_measurement(Pfeiffer_VGC_Measure(self)).start()
         
         from ScopeFoundryHW.ALD.Seren.seren_measure import Seren_Measure
-        self.add_measurement(Seren_Measure(self))
+        self.add_measurement(Seren_Measure(self)).start()
         
         from ScopeFoundryHW.ALD.ALD_recipes.ALD_functions import ALD_routine
         self.add_measurement(ALD_routine(self))
         
         from ScopeFoundryHW.ALD.ALD_recipes.ALD_params_measure import ALD_params
         self.add_measurement(ALD_params(self))
+        
+        
         
 if __name__ == '__main__':
     import sys
