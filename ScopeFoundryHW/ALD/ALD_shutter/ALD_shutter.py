9acbe50bd1fbbd524746e9b9505830dba71df933
Alan Buckley <alanbuckley@lbl.gov>
2018-05-08 11:56:23 -0700

    [ALD] ALD Shutter. Added debug mode functionality to shutter module. Wrote shutter toggle function.

--- ScopeFoundryHW/ALD/ALD_shutter/ALD_shutter.py
+++ ScopeFoundryHW/ALD/ALD_shutter/ALD_shutter.py
@@ -18,16 +18,29 @@ class ALD_Shutter(HardwareComponent):
         
         self.settings.New('port', dtype=str, initial='COM5', ro=False)
         self.settings.New('position', dtype=int, initial=0, ro=False)
+        self.settings.New('shutter_open', dtype=bool, initial=False, ro=False)
         
     def connect(self):
+
+        self.settings['debug_mode'] = True
+        self.servo = ShutterServoArduino(port=self.settings['port'], debug=self.settings['debug_mode'])
         
-        self.servo = ShutterServoArduino(port=self.settings['port'], debug=self.debug_mode.val)
-        
-        self.settings.position.connect_to_hardware(write_func=self.servo.write_position,
-                                                   read_func=self.servo.read_position)
+        if self.settings['debug_mode']:
+            print('Debug mode enabled. Bypassing connection')
+        else:                                   
+            self.settings.position.connect_to_hardware(write_func=self.servo.write_position,
+                                               read_func=self.servo.read_position)
+    
+    def shutter_toggle(self):
+        self.settings['shutter_open'] = not self.settings['shutter_open']
+        if self.settings['shutter_open']:
+            self.settings['position'] = 0
+        else:
+            self.settings['position'] = 165
+    
     
     def disconnect(self):
         self.settings.disconnect_all_from_hardware()
-        if hasattr(self, 'servo'):
+        if hasattr(self, 'servo') and not self.settings['debug_mode']:
             self.servo.close()
             del self.servo
