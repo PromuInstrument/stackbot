729bed14de29b0ff316130e5c84052096f67c112
Alan Buckley <alanbuckley@lbl.gov>
2018-05-18 15:07:08 -0700

    [ALD] Seren. Moved connection verification code to seren measure. Renamed Seren measurement.

--- ScopeFoundryHW/ALD/Seren/seren_measure.py
+++ ScopeFoundryHW/ALD/Seren/seren_measure.py
@@ -9,16 +9,17 @@ from ScopeFoundry import Measurement
 from PyQt5 import QtWidgets
 import time
 
-class Seren_Measure(Measurement):
+class Seren(Measurement):
     
-    name = "seren_measure"
+    name = "seren"
     
     def __init__(self, app):
         Measurement.__init__(self, app)
         
     def setup(self):
-        self.seren_hw = self.app.hardware['seren_hw']
-        
+        self.seren = self.app.hardware['seren_hw']
+        self.psu_connected = None
+        self.psu_check()
         self.ui_enabled = False
         
         if self.ui_enabled:
@@ -26,12 +27,24 @@ class Seren_Measure(Measurement):
             self.layout = QtWidgets.QVBoxLayout()
             self.ui.setLayout(self.layout)
     
+    def psu_check(self):
+        if hasattr(self.seren, 'seren'):
+            resp = self.seren.seren.write_cmd('R')
+            if resp == '':
+                self.psu_connected = False
+            else:
+                self.psu_connected = True
+        else:
+            print('Seren serial object not created. Try connecting HW component.')
         
         
     def run(self):
-        dt = 0.05
+        dt = 0.1
         while not self.interrupt_measurement_called:
-            self.seren_hw.settings.forward_power.read_from_hardware()
-            time.sleep(dt)
-            self.seren_hw.settings.reflected_power.read_from_hardware()
-            time.sleep(dt)
+            if self.psu_connected:
+                self.seren_hw.settings.forward_power.read_from_hardware()
+                time.sleep(dt)
+                self.seren_hw.settings.reflected_power.read_from_hardware()
+                time.sleep(dt)
+            else:
+                break
