3f9314fd02f4efe4e8977420793a0b36a93f381f
Alan Buckley <alanbuckley@lbl.gov>
2018-06-15 15:07:07 -0700

    [ALD] ALD params. Commented out single recipe start button object.

--- ScopeFoundryHW/ALD/ALD_recipes/ALD_params_measure.py
+++ ScopeFoundryHW/ALD/ALD_recipes/ALD_params_measure.py
@@ -411,7 +411,7 @@ class ALD_params(Measurement):
         
 
         
-        self.single_start_button = QtWidgets.QPushButton('Start 1 Recipe')
+#         self.single_start_button = QtWidgets.QPushButton('Start 1 Recipe')
 #         self.single_start_button.clicked.connect(self.recipe.load_single_recipe)
 #         self.recipe_panel.layout().addWidget(self.single_start_button, 0, 0)
         
