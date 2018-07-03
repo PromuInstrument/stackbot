0631081ac53dda3848269dfde3e68d7c77d1c20d
Edward Barnard <esbarnard@lbl.gov>
2018-07-03 13:51:01 -0700

    [pololu_servo] Update HW component with scaled position, switch raw units

--- ScopeFoundryHW/pololu_servo/single_servo_hw.py
+++ ScopeFoundryHW/pololu_servo/single_servo_hw.py
@@ -7,21 +7,42 @@ class PololuMaestroServoHW(HardwareComponent):
     
     
     def setup(self):        
-        self.settings.New(name='port', initial='COM1', dtype=str, ro=False)
-        self.settings.New(name='servo_num', dtype=int, initial=1, ro=False)
-        self.settings.New(name="servo_position", dtype=int, 
-                                        vmin=500, vmax=2500, ro=False)
+        S = self.settings
         
+        S.New(name='port', initial='COM1', dtype=str, ro=False)
+        S.New(name='servo_num', dtype=int, initial=0, ro=False)
+        S.New(name="raw_position", dtype=int,  ro=False)
+        
+        S.New('raw_min', dtype=float, initial=2000, ro=False)
+        S.New('raw_max', dtype=float, initial=10000, ro=False)
+        S.New('pos_scale', dtype=float, initial=180, ro=False)
+        S.New('position', dtype=float, ro=False)
+        
+        
+        def pos_rev_func(new_pos, old_vals):
+            raw, rmin, rmax, scale = old_vals
+            new_raw = rmin + new_pos/scale*(rmax-rmin)
+            return (new_raw, rmin, rmax, scale)
+
+        S.position.connect_lq_math( (S.raw_position, S.raw_min, S.raw_max, S.pos_scale),
+                                    func=lambda raw, rmin, rmax, scale: scale*( (raw - rmin)/(rmax-rmin) ),
+                                    reverse_func= pos_rev_func)
+        
+        
+        S.New('jog_step', dtype=float, initial=10.0)
+        
+        self.add_operation("Jog +", self.jog_fwd)
+        self.add_operation("Jog -", self.jog_bkwd)
     
     def connect(self):
         self.dev = PololuMaestroDevice(port=self.settings['port'])
 
-        self.settings.servo_position.connect_to_hardware(
+        self.settings.raw_position.connect_to_hardware(
             read_func=lambda: self.dev.read_position(self.settings['servo_num']),
             write_func=lambda pos: self.dev.write_position(self.settings['servo_num'],pos)
             )
         
-        self.settings.servo_position.read_from_hardware()
+        self.settings.raw_position.read_from_hardware()
         
     def disconnect(self):
         
@@ -30,3 +51,13 @@ class PololuMaestroServoHW(HardwareComponent):
         if hasattr(self, 'dev'):
             self.dev.close()
             del self.dev
+            
+    
+    def jog_fwd(self):
+        S = self.settings
+        S['position'] += S['jog_step']
+    
+    def jog_bkwd(self):
+        S = self.settings
+        S['position'] -= S['jog_step']
+    
