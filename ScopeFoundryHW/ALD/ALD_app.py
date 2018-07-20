7669302d130ee60b0fe38338ed544c4df8db1054
Alan Buckley <alanbuckley@lbl.gov>
2018-07-20 14:29:15 -0700

    [ALD] ALD App. Autostart of all modules.

--- ScopeFoundryHW/ALD/ALD_app.py
+++ ScopeFoundryHW/ALD/ALD_app.py
@@ -22,55 +22,55 @@ class ALD_App(BaseMicroscopeApp):
         
         from ScopeFoundryHW.ALD.ALD_relay.ald_relay_hardware import ALDRelayHW
         relay = self.add_hardware(ALDRelayHW(self))
-        relay.settings['connected'] = False
+        relay.settings['connected'] = True
         
         from ScopeFoundryHW.ALD.ALD_shutter.ALD_shutter import ALD_Shutter
-        self.add_hardware(ALD_Shutter(self)).settings['connected'] = False
+        self.add_hardware(ALD_Shutter(self)).settings['connected'] = True
         
         from ScopeFoundryHW.ALD.ellipsometer.ellipsometer_hw import EllipsometerHW
         self.add_hardware(EllipsometerHW(self)).settings['connected'] = True
         
         from ScopeFoundryHW.ALD.Lovebox.lovebox_hw import LoveboxHW
-        self.add_hardware(LoveboxHW(self)).settings['connected'] = False
+        self.add_hardware(LoveboxHW(self)).settings['connected'] = True
         
         from ScopeFoundryHW.ALD.MKS_146.mks_146_hw import MKS_146_Hardware
-        self.add_hardware(MKS_146_Hardware(self)).settings['connected'] = False
+        self.add_hardware(MKS_146_Hardware(self)).settings['connected'] = True
          
         from ScopeFoundryHW.ALD.MKS_600.mks_600_hw import MKS_600_Hardware
-        self.add_hardware(MKS_600_Hardware(self)).settings['connected'] = False
+        self.add_hardware(MKS_600_Hardware(self)).settings['connected'] = True
         
         from ScopeFoundryHW.ALD.pfeiffer_vgc.pfeiffer_vgc_hw import Pfeiffer_VGC_Hardware
-        self.add_hardware(Pfeiffer_VGC_Hardware(self)).settings['connected'] = False
+        self.add_hardware(Pfeiffer_VGC_Hardware(self)).settings['connected'] = True
           
         from ScopeFoundryHW.ALD.Seren.seren_hw import Seren_HW
-        self.add_hardware(Seren_HW(self)).settings['connected'] = False
+        self.add_hardware(Seren_HW(self)).settings['connected'] = True
         
         from ScopeFoundryHW.ALD.ALD_relay.ald_relay_measure import ALDRelayMeasure
-        self.add_measurement(ALDRelayMeasure(self))#.start()
+        self.add_measurement(ALDRelayMeasure(self)).start()
         
         from ScopeFoundryHW.ALD.ellipsometer.ellipsometer_measure import Ellipsometer_Measure
         self.add_measurement(Ellipsometer_Measure(self)).start()
           
         from ScopeFoundryHW.ALD.Lovebox.lovebox_measure import LoveboxMeasure
-        self.add_measurement(LoveboxMeasure(self))#.start()
+        self.add_measurement(LoveboxMeasure(self)).start()
         
         from ScopeFoundryHW.ALD.MKS_146.mks_146_measure import MKS_146_Measure
-        self.add_measurement(MKS_146_Measure(self))#.start()
+        self.add_measurement(MKS_146_Measure(self)).start()
         
         from ScopeFoundryHW.ALD.MKS_600.mks_600_measure import MKS_600_Measure
-        self.add_measurement(MKS_600_Measure(self))#.start()
+        self.add_measurement(MKS_600_Measure(self)).start()
          
         from ScopeFoundryHW.ALD.pfeiffer_vgc.pfeiffer_vgc_measure import Pfeiffer_VGC_Measure
-        self.add_measurement(Pfeiffer_VGC_Measure(self))#.start()
+        self.add_measurement(Pfeiffer_VGC_Measure(self)).start()
         
         from ScopeFoundryHW.ALD.Seren.seren_measure import Seren
-        self.add_measurement(Seren(self))#.start()
+        self.add_measurement(Seren(self)).start()
 
         from ScopeFoundryHW.ALD.ALD_recipes.ALD_recipe import ALD_Recipe
         self.recipe_measure = self.add_measurement(ALD_Recipe(self))
         
         from ScopeFoundryHW.ALD.ALD_recipes.ALD_display import ALD_Display
-        self.display_measure = self.add_measurement(ALD_Display(self))#.start()
+        self.display_measure = self.add_measurement(ALD_Display(self)).start()
         
         self.recipe_measure.load_display_module()
 
