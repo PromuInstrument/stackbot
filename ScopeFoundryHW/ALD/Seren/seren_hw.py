28abaa38d417eb3ee281992a5c97a2e76c8d8ec5
Alan Buckley <alanbuckley@lbl.gov>
2018-08-06 16:43:23 -0700

    [ALD] Seren. Added docstrings to hardware level module.

--- ScopeFoundryHW/ALD/Seren/seren_hw.py
+++ ScopeFoundryHW/ALD/Seren/seren_hw.py
@@ -41,33 +41,88 @@ class Seren_HW(HardwareComponent):
         self.serial_toggle(True)
 
     def serial_toggle(self, status):
+        """Sets serial or front panel control.
+        
+        Connected to *LoggedQuantity* 
+        :attr:`self.settings.enable_serial`
+        
+        =============  ==========  ==========================================================
+        **Arguments**  **type**    **Description**
+        status         bool        Serial enabled/disabled
+        =============  ==========  ==========================================================
+        """
         if status:
             self.seren.set_serial_control()
         else:
             self.seren.set_front_panel_control()
     
     def RF_state(self, status):
+        """
+        Enables or disables RF source.
+        
+        Connected to *LoggedQuantity*
+        :attr:`self.settings.RF_enable`
+        
+        =============  ==========  ==========================================================
+        **Arguments**  **type**    **Description**
+        status         bool        RF emitter enabled/disbaled
+        =============  ==========  ==========================================================
+        """
         if status:
             self.seren.emitter_on()
         else:
             self.seren.emitter_off()
 
     def RF_toggle(self):
+        """Toggles RF enable with each call by negating the value of
+        *LoggedQuantity*
+        :attr:`self.settings.RF_enable`
+        """
         self.settings['RF_enable'] = not self.settings['RF_enable']
 
     
     def write_fp(self, power):
-        self.seren.write_forward(power)
-        self.read_from_hardware()
+        """
+        Writes forward power setting to Seren PSU.
+        
+        Connected to *LoggedQuantity*
+        :attr:`self.settings.set_forward_power`
+        
+        =============  ==========  ==========================================================
+        **Arguments**  **type**    **Description**
+        power          int         RF power setpoint in Watts
+        =============  ==========  ==========================================================
+        """
+        self.seren.write_forward(int(power))
         
     def read_fp(self):
+        """
+        Reads set forward power from Seren PSU.
+        
+        Connected to *LoggedQuantity*
+        :attr:`self.settings.forward_power_readout`
+        
+        :returns: int. Forward power in Watts.
+        """
         return self.seren.read_forward()
     
     def read_rp(self):
+        """
+        Reads reflected power from Seren PSU.
+        
+        Connected to *LoggedQuantity*
+        :attr:`self.settings.reflected_power`
+        
+        :returns: int. Reflected power in Watts.
+        """
         resp = self.seren.read_reflected()
         return resp
     
     def disconnect(self):
+        """
+        Properly disconnects logged quantities from module functions.
+        Closes serial connection to Seren PSU.
+        """
         self.settings.disconnect_all_from_hardware()
         if self.seren is not None:
             self.seren.close()
