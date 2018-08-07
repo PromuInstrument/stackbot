f583828f7f500f430f44075725c47a4c039ba7ba
Alan Buckley <alanbuckley@lbl.gov>
2018-08-07 15:33:50 -0700

    [ALD] Pfeiffer VGC. Added docstrings to SQLite wrapper module.

--- ScopeFoundryHW/ALD/pfeiffer_vgc/vgc_sqlite_core.py
+++ ScopeFoundryHW/ALD/pfeiffer_vgc/vgc_sqlite_core.py
@@ -13,11 +13,13 @@ import random
 
 class SQLite_Wrapper(object):
 
-	def __init__(self):
-		self.conn = sqlite3.connect('pressure_log.db', detect_types=sqlite3.PARSE_DECLTYPES)
-		self.c = self.conn.cursor()
+	"""
+	This module is a SQLite wrapper which records pressure entries into a database (.db) file. 
+	
+	The module currently isn't used. 
+	"""
 
-	def connect(self):
+	def __init__(self):\
 		self.conn = sqlite3.connect('pressure_log.db', detect_types=sqlite3.PARSE_DECLTYPES)
 		self.c = self.conn.cursor()
 
@@ -30,12 +32,23 @@ class SQLite_Wrapper(object):
 	
 
 	def data_entry(self, p1, p2, p3):
+		"""
+		Records three pressures into database.
+
+		=============  ===============  =========================================
+		**Arguments**  **Type**		 	**Description**
+		p1			   float			Pressure TKP1 (Pirani Gauge)
+		p2			   float			Pressure TKP2 (Pirani Gauge)
+		p3			   float			Pressure PKR3 (Compact Full Range)
+		=============  ===============  =========================================
+		"""
 		_datetime = datetime.datetime.now()
 		self.c.execute("INSERT INTO PressureTable (time, TKP1, TKP2, PKR3) VALUES (?, ?, ?, ?)",
 				  (_datetime, p1, p2, p3))
 		self.conn.commit()
 
 	def check_db(self):
+		"""Checks if database is empty."""
 		self.c.execute('SELECT Count(*) FROM PressureTable')
 		size = self.c.fetchone()[0]
 		if size > 0:
@@ -45,21 +58,39 @@ class SQLite_Wrapper(object):
 			pass
 
 	def read_db_all(self):
+		"""Select database contents in their entirety.
+		:returns: list of tuples. Each tuple contains a datetime object, \
+		and three floats corresponding to pressures"""
 		self.c.execute("""SELECT * FROM PressureTable""")
 		data = self.c.fetchall()
 		return(data)
 
 	def filter_time(self, t1, t2):
+		"""
+		Filter events by time
+		:returns: list of tuples. Each tuple contains a datetime object, \
+		and three floats corresponding to pressures
+		"""
 		self.c.execute("""SELECT * FROM PressureTable WHERE time BETWEEN ? AND ?""", (t1, t2))
 		data = self.c.fetchall()
 		return(data)
 
 	def filter_pressures_time(self, t1, t2):
+		"""
+		Filter recorded pressures by time
+		:returns: list of tuples. Each tuple contains \
+		three floats corresponding to pressures
+		"""
 		self.c.execute("""SELECT TKP1, TKP2, PKR3 FROM PressureTable WHERE time BETWEEN ? AND ?""", (t1, t2))
 		data = self.c.fetchall()
 		return(data)
 
 	def read_all_dates(self):
+		"""
+		Lists all date time objects.
+		
+		:returns: list of datetime objects.
+		"""
 		dates = []
 		data = self.read_db_all()
 		for i in data:
@@ -67,8 +98,10 @@ class SQLite_Wrapper(object):
 		return(dates)
 		
 	def bobby_tables(self):
+		"""Drop (Delete) PressureTable."""
 		self.c.execute('''DROP TABLE IF EXISTS PressureTable''')
 			
 	def closeout(self):
+		"""Close connection to SQLite database."""
 		self.c.close()
 		self.conn.close()
