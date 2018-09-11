a48ee67ace2e975835305cd45fe47da5ef573e5c
Alan Buckley <alanbuckley@lbl.gov>
2018-09-11 13:32:17 -0700

    [ALD] NI MFC. Added docstrings and renamed logged quantities for sake of clarity for the end user. Converted some logged quantities to static addresses and fixed values, both stored to variables.

--- ScopeFoundryHW/ALD/NI_MFC/ni_mfc_hardware.py
+++ ScopeFoundryHW/ALD/NI_MFC/ni_mfc_hardware.py
@@ -18,20 +18,18 @@ class NI_MFC(HardwareComponent):
     name = 'ni_mfc'
     
     def __init__(self, app, debug=False):
-        self.line_names = "01234567"
+        self.line_names = ['valve_close', 'valve_open']
         HardwareComponent.__init__(self, app, debug)
         
+    def setup(self):        
+        self.dio_port = 'Dev1/port0/line0:7'
         
+        self.set_voltage_channel = '/Dev1/ao0'
         
-    def setup(self):
-        self.settings.New('DIO_port', dtype=str, initial='Dev1/port0/line0:7')
-        self.settings.New('dac_sp', dtype=float, ro=False, unit='V')
-        self.settings.New('dac_channel', dtype=str, initial='/Dev1/ao0')
-        self.settings.New('adcV', dtype=float, ro=True, unit='V')
-        self.settings.New('adc_channel', dtype=str, initial='/Dev1/ai0')
-        self.settings.New('adc_range', dtype=float, initial=10, unit='V')
-        self.settings.New('adc_config', dtype=str, initial='default', 
-                          choices=['default', 'rse', 'nrse', 'diff', 'pdiff'])
+        self.flow_voltage_range = 5.0
+        self.flow_voltage_channel = '/Dev1/ai0'
+        
+        self.adc_config = 'diff'
         
         self.line_pins = dict()
         for pin_i, line_name in enumerate(self.line_names):
@@ -39,43 +37,76 @@ class NI_MFC(HardwareComponent):
                 continue
             self.line_pins[line_name] = pin_i
             self.settings.New(name=line_name, dtype=bool)
-            
-    def connect(self):
-        self.dio_task = mx.Task()
-        self.dio_task.CreateDOChan(self.settings['DIO_port'], "", mx.DAQmx_Val_ChanForAllLines)
         
-        self.dac_task = NI_DacTask(channel=self.settings['dac_channel'], name='ni_dac')
+        self.settings.New(name='write_flow', dtype=float, initial=0.0, vmin=0.0, vmax = 20.0, ro=False)
+        self.settings.New(name='read_flow', dtype=float, initial=0.0, ro=False)
+
+        
+    def connect(self):
+        """
+        Creates and instantiates NI task objects thereby creating a connection to the NI ADC/DAC controller
+        Connects logged quantities to their respective functions.
+        """
+        self.dio_task = mx.Task()
+        self.dio_task.CreateDOChan(self.dio_port, "", mx.DAQmx_Val_ChanForAllLines)
+        
+        self.dac_task = NI_DacTask(channel=self.set_voltage_channel, name='ni_dac')
         self.dac_task.set_single()
         self.dac_task.start()
         
-        self.adc_task = NI_AdcTask(channel=self.settings['adc_channel'], 
-                                   range=self.settings['adc_range'], name='ni_adc',
-                                   terminalConfig=self.settings['adc_config'])
+        self.adc_task = NI_AdcTask(channel=self.flow_voltage_channel, 
+                                   range=self.flow_voltage_range, name='ni_adc',
+                                   terminalConfig=self.adc_config)
         self.adc_task.set_single()
         self.adc_task.start()
         
         
-        for line_name, pin in self.line_pins.items():
+        for line_name, _ in self.line_pins.items():
             self.settings.get_lq(line_name).connect_to_hardware(
                 write_func=self.write_digital_lines)
-        self.settings.get_lq('dac_sp').connect_to_hardware(
-                                        write_func=self.dac_task.set)
-        self.settings.get_lq('adcV').connect_to_hardware(
-                                        read_func=self.read_adc_single)
         
+        self.settings.get_lq('write_flow').connect_to_hardware(
+                                            write_func=self.write_flow)
+        self.settings.get_lq('read_flow').connect_to_hardware(
+                                            read_func=self.read_flow)
+
+    def write_flow(self, flow):
+        """
+        Converts user supplied flow value and scales it to a voltage 
+        read by the MFC analog interface.
+        Our particular MFC has a flow rate of 20 sccm, which corresponds to a signal of +5V.
+        """
+        voltage = (1/4)*flow
+#         voltage = 1*flow
+        self.dac_task.set(voltage)
+    
+    def read_flow(self):
+        """
+        Reads voltage from MFC analog interface. Scales voltage to flow value.
+        Our particular MFC has a flow rate of 20 sccm, which corresponds to a signal of +5V.
+        :returns: (float) Flow rate detected in MFC in units of sccm.
+        """
+        voltage = self.read_adc_single()
+        flow = 4*voltage
+#         flow = voltage
+        return flow
+
+            
     def write_digital_lines(self, x=None):
-        writeArray = np.zeros(8, dtype=mx.c_uint8)
+        """Writes boolean values to digital in/out channels. """
+        self.writeArray = np.zeros(8, dtype=mx.c_uint8)
         for line_name, pin in self.line_pins.items():
             pin_bool = int(self.settings[line_name])
-            writeArray[pin] = pin_bool
-        
+            self.writeArray[pin] = pin_bool        
         sampsPerChanWritten = mx.c_int32()
-        
         self.dio_task.WriteDigitalLines(numSampsPerChan=1, autoStart=True, timeout=0,
                                     dataLayout=mx.DAQmx_Val_GroupByChannel, 
-                                    writeArray=writeArray, sampsPerChanWritten=mx.byref(sampsPerChanWritten), reserved=None)
+                                    writeArray=self.writeArray, sampsPerChanWritten=mx.byref(sampsPerChanWritten), reserved=None)
+
         
     def read_adc_single(self):
+        """Reads voltage off analog to digital converter. In the case of this module, 
+        this channel reads the value of our MFC's Flow Signal Output."""
         resp = self.adc_task.get()
         if self.debug_mode.val:
             self.log.debug('read_adc_single resp: {}'.format(resp))
@@ -83,6 +114,9 @@ class NI_MFC(HardwareComponent):
     
     
     def disconnect(self):
+        """Disconnects logged quantities from their respective functions
+        Stops NI Tasks and removes Task objects.
+        """
         self.settings.disconnect_all_from_hardware()
         
         if hasattr(self, 'task'):
