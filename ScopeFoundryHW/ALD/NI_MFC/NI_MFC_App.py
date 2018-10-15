5a97613319e947ed50c040597a024e25141c35b8
Alan Buckley <alanbuckley@lbl.gov>
2018-10-14 17:50:54 -0700

    [ALD] NI MFC. Table formatting looks screwed up due to the way eclipse renders its tabs and spacing.

--- ScopeFoundryHW/ALD/NI_MFC/NI_MFC_App.py
+++ ScopeFoundryHW/ALD/NI_MFC/NI_MFC_App.py
@@ -12,18 +12,37 @@ import logging
 
 
 class NI_MFC_App(BaseMicroscopeApp):
-    
-    name="ni_mfc_app"
-    
-    def setup(self):
-        
-        from ScopeFoundryHW.ALD.NI_MFC.ni_mfc_hardware import NI_MFC
-        self.add_hardware(NI_MFC(self))
-        
-        from ScopeFoundryHW.ALD.NI_MFC.ni_mfc_measure import NI_MFC_Measure
-        self.add_measurement(NI_MFC_Measure(self))
-    
+	
+	"""Module which controls MKS 1479 Mass Flow Controller via its analog
+	DA-15 connector with the use of a National Instruments USB-6001 
+	Multifunction I/O device.
+	
+	This device regulates analog voltages needed to adjust or read MFC flow values 
+	as well as opening or closing its valve.
+	
+	
+	+-------------------+----------------------------+------------------------------------------------------+
+	|	Module Type		|	Module Name				 |	Description											|
+	+===================+============================+======================================================+
+	|	Hardware		|	NI_MFC					 |	Establishes Read/Write *LoggedQuantities*			|
+	|					|							 |	Wraps NI DAQ functions needed to carry out commands.|
+	+-------------------+----------------------------+------------------------------------------------------+
+	|	Measurement		|	NI_MFC_Measure			 |	Actively reads current flow rate in					|
+	|					|							 |	MFC.												|
+	+-------------------+----------------------------+------------------------------------------------------+
+	"""
+	
+	name="ni_mfc_app"
+	
+	def setup(self):
+		
+		from ScopeFoundryHW.ALD.NI_MFC.ni_mfc_hardware import NI_MFC
+		self.add_hardware(NI_MFC(self))
+		
+		from ScopeFoundryHW.ALD.NI_MFC.ni_mfc_measure import NI_MFC_Measure
+		self.add_measurement(NI_MFC_Measure(self))
+	
 if __name__ == '__main__':
-    import sys
-    app = NI_MFC_App(sys.argv)
-    sys.exit(app.exec_()) 
+	import sys
+	app = NI_MFC_App(sys.argv)
+	sys.exit(app.exec_()) 
