9e66b6707c484fd707bdf839c5100f3b971ca276
alanbuckley <alanbuckley@lbl.gov>
2018-11-30 18:15:00 -0800

    [ALD] shutter_servo_arduino. Cleaned up PySerial's mess (encoding issues).

--- ScopeFoundryHW/shutter_servo_arduino/shutter_servo_arduino_interface.py
+++ ScopeFoundryHW/shutter_servo_arduino/shutter_servo_arduino_interface.py
@@ -35,7 +35,8 @@ class ShutterServoArduino(object):
         if self.debug: logger.debug( "send_cmd:" + repr(cmd))
         full_cmd = cmd + "\r\n"
         with self.lock:
-            self.ser.write(full_cmd)
+#             self.ser.write(full_cmd)
+            self.ser.write(full_cmd.encode())
     
     def ask(self, cmd):
         if self.debug: logger.debug( "ask:" +  repr(cmd) )
