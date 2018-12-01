5a5022a5a6d0658b6d83a210945994f64fc78b47
alanbuckley <alanbuckley@lbl.gov>
2018-11-30 18:13:16 -0800

    [ALD] Pfeiffer VGC. Cleaned up PySerial's mess (encoding issues).

--- ScopeFoundryHW/ALD/pfeiffer_vgc/pfeiffer_vgc_interface.py
+++ ScopeFoundryHW/ALD/pfeiffer_vgc/pfeiffer_vgc_interface.py
@@ -54,7 +54,7 @@ class Pfeiffer_VGC_Interface(object):
         message = cmd+'\r\n'
         
         with self.lock:
-            self.ser.write(message)
+            self.ser.write(message.encode())
             resp = self.ser.readline()
         
         if self.debug:
@@ -62,7 +62,8 @@ class Pfeiffer_VGC_Interface(object):
             print("ask_cmd resp:", resp)
         if resp == b"\x06\r\n":
             with self.lock:
-                self.ser.write(b"\x05".decode()+"\r\n")
+#                 self.ser.write(b"\x05".decode()+"\r\n")
+                self.ser.write(b"\x05"+"\r\n".encode())
                 resp = self.ser.readline()
             return resp
         else: 
