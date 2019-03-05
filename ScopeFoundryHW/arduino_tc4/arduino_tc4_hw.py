d2f879b9a429c380d70d6ebd567a855699d298a0
Benedikt Ursprung <bursprung@lbl.gov>
2019-03-04 19:44:49 -0800

    [logged_quantity] hardware_read_func and hardware_set_func can no longer be set at initialization of the logged_quantity.

--- ScopeFoundryHW/arduino_tc4/arduino_tc4_hw.py
+++ ScopeFoundryHW/arduino_tc4/arduino_tc4_hw.py
@@ -12,11 +12,11 @@ class ArduinoTc4HW(HardwareComponent):
     def setup(self):
         self.ser_port = self.settings.New('ser_port', dtype=str, initial='COM12')
         self.temp = self.settings.New('temperature', float, initial=-1.,ro=True, 
-                                      spinbox_decimals=3, unit='C',
-                                      hardware_read_func = self.read_temp) 
+                                      spinbox_decimals=3, unit='C') 
         
     def connect(self):
         self.dev = ArduinoTc4Dev(self.ser_port.val)
+        self.temp.connect_to_hardware(read_func=self.read_temp, write_func=None)
         
     def read_temp(self):
         return self.dev.read_temp()
