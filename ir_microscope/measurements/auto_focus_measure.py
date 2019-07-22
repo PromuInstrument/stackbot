813dbd4b72323d4c9fe3f1d6e05a234cac9cf9bb
Benedikt Ursprung <bursprung@lbl.gov>
2019-07-21 20:46:58 -0700

    [ir_microscope/measurements/autofocus_measure] can now optimize on andor detectors and fine optimization is optional.

--- ir_microscope/measurements/auto_focus_measure.py
+++ ir_microscope/measurements/auto_focus_measure.py
@@ -29,6 +29,7 @@ class AutoFocusMeasure(Measurement):
                          ('hydraharp_CountRate1','hardware/hydraharp/CountRate1'),
                          ('picoharp_CountRate0','hardware/hydraharp/count_rate'),
                          ('toupcam_inv_FWHM','measure/toupcam_spot_optimizer/inv_FWHM'),
+                         ('andor(CONTINUOUSLY)_count_rate','measure/andor_ccd_readout/count_rate'),
                          ]
         
         self.settings.New('optimization_quantity', dtype=str, choices=self.optimization_quantity_choices,
@@ -41,6 +42,8 @@ class AutoFocusMeasure(Measurement):
         self.settings.New('z_correction', initial=0.0,**lq_kwargs)     
         self.settings.New('sampling_period', float, initial=0.050, unit='s', si=True)
         self.settings.New('N_samples', int, initial=10)
+        
+        self.settings.New('use_fine_optimization', bool, initial=True)
         
         self.z_optimal_history = []
         self.t0 = time.time()
@@ -60,7 +63,7 @@ class AutoFocusMeasure(Measurement):
                                              'N_samples', 'sampling_period']) )
         settings_layout.addWidget( S.New_UI(['use_current_z_as_center', 
                                              'z_center', 'z_span', 'z_num']) )
-        settings_layout.addWidget( S.New_UI(['z_correction']) )
+        settings_layout.addWidget( S.New_UI(['z_correction', 'use_fine_optimization']) )
                                    
                                    
         ## Add plot and plot items
@@ -71,7 +74,7 @@ class AutoFocusMeasure(Measurement):
         self.plot_line = self.plot.plot()
         self.plot_line_fine = self.plot.plot(pen='r')
         self.line_z_center_start = pg.InfiniteLine(angle=0, movable=False, pen='b')
-        self.line_z_center_coarse = pg.InfiniteLine(angle=0, movable=False, pen='y')
+        self.line_z_center_coarse = pg.InfiniteLine(angle=0, movable=False)
         self.line_z_center_fine = pg.InfiniteLine(angle=0, movable=False, pen='r')
     
         self.plot.addItem(self.line_z_center_start, ignoreBounds=True)
@@ -94,6 +97,8 @@ class AutoFocusMeasure(Measurement):
             
     def run(self):
         S = self.settings
+        
+        # stage
         z_position = self.app.hardware['attocube_xyz_stage'].settings.z_position
         z_target_position = self.app.hardware['attocube_xyz_stage'].settings.z_target_position
 
@@ -101,12 +106,9 @@ class AutoFocusMeasure(Measurement):
         self.optimization_lq = self.app.lq_path(S['optimization_quantity']) 
 
         if S['use_current_z_as_center']:
-            S['z_center'] = z_position.val
-        
-        # switch to apd using flip mirror
-        #old_flip_position = self.app.hardware['flip_mirrors'].settings['apd_flip'] 
-        #self.app.hardware['flip_mirrors'].settings['apd_flip'] = True
-        
+            S['z_center'] = z_position.read_from_hardware()
+
+
         # create data arrays
         self.z_array = np.linspace(S['z_center']-0.5*S['z_span'],
                                    S['z_center']+0.5*S['z_span'],
@@ -123,10 +125,8 @@ class AutoFocusMeasure(Measurement):
         # for loop through z-array
         for i, z in enumerate(self.z_array):
             self.set_progress(i*100.0/S['z_num']/3)
-            # check for interrupt:
             if self.interrupt_measurement_called:
                 break
-            # move z
             z_target_position.update_value(z)
             self.quantity_records[i] = self.get_optimization_quantity()
         
@@ -141,15 +141,14 @@ class AutoFocusMeasure(Measurement):
 
         z_target_position.update_value(self.z_array_fine[0])
         time.sleep(0.1)
-        
-        for i, z in enumerate(self.z_array_fine):
-            self.set_progress((S['z_num']+i)*100.0/S['z_num']/3)
-            if self.interrupt_measurement_called:
-                break            
-            # move z
-            z_target_position.update_value(z)
-            self.quantity_records_fine[i] = self.get_optimization_quantity()       
-            self.z_center_fine = self.z_array_fine[np.argmax(self.quantity_records_fine)]
+        if S['use_fine_optimization']:
+            for i, z in enumerate(self.z_array_fine):
+                self.set_progress((S['z_num']+i)*100.0/S['z_num']/3)
+                if self.interrupt_measurement_called:
+                    break            
+                z_target_position.update_value(z)
+                self.quantity_records_fine[i] = self.get_optimization_quantity()       
+                self.z_center_fine = self.z_array_fine[np.argmax(self.quantity_records_fine)]
         
         if self.interrupt_measurement_called:
             z_target_position.update_value(original_z)
@@ -164,7 +163,7 @@ class AutoFocusMeasure(Measurement):
         self.plot.enableAutoRange()
 
         self.plot_line.setData(self.quantity_records, self.z_array)
-        if hasattr(self, 'apd_counts_fine'):
+        if hasattr(self, 'quantity_records_fine'):
             self.plot_line_fine.setData(self.quantity_records_fine, self.z_array_fine)
         
         S = self.settings
